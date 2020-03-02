61a28c98e37333c5eec2f8fb4fdcdf7f858421f9
Scott-Scott-equals-new-Scott <58554273+Scott-Scott-equals-new-Scott@users.noreply.github.com>
2020-03-02 15:19:58 +0000

    Update Tiger.cs

--- Tiger.cs
+++ Tiger.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System;
 
 namespace FelineProjecr
 {
-    class Tiger
+    class Tiger : Feline, IClimb, ICamo, ISwim
     {
+        public void Camo()
+        {
+            Console.WriteLine($"{this.GetType().Name} is camouflaged");
+        }
+
+        public void Climb()
+        {
+            Console.WriteLine($"{this.GetType().Name} is climbing");
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine($"{this.GetType().Name} is climbing");
+        }
     }
 }
