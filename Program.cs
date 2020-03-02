e327921d95fae459a877f7da80cc15b9133c9e9d
methodoverloading <58049363+methodoverloading@users.noreply.github.com>
2020-03-02 15:52:09 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Reflection;
 
 namespace FelineProjecr
 {
@@ -6,7 +7,24 @@ namespace FelineProjecr
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            foreach (Type type in Assembly.GetExecutingAssembly().GetTypes())
+            {
+                if (type.Name == "Program")
+                    continue;
+                if (type.IsAbstract)
+                    continue;
+                
+                Feline feline = (Feline)Activator.CreateInstance(type);
+
+                foreach (MethodInfo method in type.GetMethods())
+                {
+                    if (method.Name == "Equals" || method.Name == "GetHashCode" || method.Name == "GetType" || method.Name == "ToString")
+                        continue;
+
+                    method.Invoke(feline, null);
+                }
+                
+            }
         }
     }
 }
