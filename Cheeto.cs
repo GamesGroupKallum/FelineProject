aa83cdef737100697920cf51339bcaa7638ee53d
methodoverloading <58049363+methodoverloading@users.noreply.github.com>
2020-03-02 15:17:51 +0000

    Update Cheeto.cs

--- Cheeto.cs
+++ Cheeto.cs
@@ -4,6 +4,9 @@ namespace FelineProjecr
 {
     class Cheeto : Feline, IClimb
     {
-
+        public void Climb()
+        {
+            Console.WriteLine($"{this.GetType().Name} is climbing");
+        }
     }
 }
