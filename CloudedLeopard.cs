34b4d4d25c773dcf21e6f718f357bf93421dfa2d
GamesGroupKallum <58046421+GamesGroupKallum@users.noreply.github.com>
2020-03-02 15:39:21 +0000

    Add files via upload

---
+++ CloudedLeopard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FelineProject
+{
+    class CloudedLeopard: Feline, ICamo, IClimb, ISwim
+    {
+        public void Climb()
+        {
+            Console.WriteLine($"{this.GetType().Name} is climbing.");
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine($"{this.GetType().Name} is swimming.");
+        }
+
+        public void Camo()
+        {
+            Console.WriteLine($"{this.GetType().Name} is camoflaged.");
+        }
+    }
+}
